2f1b030b880681d89fc3e5307560873766b1576f
ElanHashem <103283907+ElanHashem@users.noreply.github.com>
2022-05-30 23:03:30 -0700

    s

---
+++ MarkDownParseTester.java
@@ -0,0 +1,17 @@
+import java.beans.Transient;
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+
+public class MarkDownParseTester {
+    
+    @Test
+    public void testSnippet1()
+    {
+        ArrayList<String> actual = new ArrayList<String>();
+        MarkDownParse.getLinks("mom");
+
+        
+    }
+}
