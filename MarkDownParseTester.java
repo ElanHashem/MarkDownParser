d5d1a42607f274be3e2ae83b464bd478afc2a898
ElanHashem <103283907+ElanHashem@users.noreply.github.com>
2022-05-30 23:07:14 -0700

    s

--- MarkDownParseTester.java
+++ MarkDownParseTester.java
@@ -1,5 +1,11 @@
+
+
 import java.beans.Transient;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import org.junit.Test;
 
@@ -7,10 +13,15 @@ import org.junit.Test;
 public class MarkDownParseTester {
     
     @Test
-    public void testSnippet1()
+    public void testSnippet1(String args[]) throws IOException
     {
-        ArrayList<String> actual = new ArrayList<String>();
-        MarkDownParse.getLinks("mom");
+        String[] expected = new String[]{"[a link](url.com)","[a link](url.com)","cod[e","code]"};
+        String contents = Files.readString(Path.of("Snippet-1.md"));
+        ArrayList<String> actual = MarkDownParse.getLinks(contents);
+        for(int i=0;i<expected.length;i++)
+        {
+            assertEquals(expected[i], actual.get(i));
+        }
 
         
     }
